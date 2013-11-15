e71ad0d21f5f7d6aec47eb1a8ac0f57b1cbe0b9e
Peter <pmansour112@gmail.com>
2013-11-15 23:46:30 +1100

    ID3 induction works, but the results are a little bit suspicious.. need a little more testing and clean up, and also need to implement classification

--- machine learning/ID3.py
+++ machine learning/ID3.py
@@ -1,4 +1,5 @@
 import math
+from collections import defaultdict
 
 # calculate the entropy for options with the given probabilities
 # eg. for a fair coin, entropy([0.5,0.5]) = 1
@@ -8,7 +9,7 @@ def entropy(probabilities):
     for P in probabilities:
         # log is undefined for non-positive numbers (so lets pretend it's zero)
         if P > 0:
-            entropy = entropy - (P * math.log(P, 2))
+            entropy -= (P * math.log(P, 2))
     return entropy
 
 # get the probabilities of each class from a list of counts of class elements
@@ -17,60 +18,205 @@ def probs_from_counts(counts):
     total = sum(counts)
     return [(1.0 * count) / total for count in counts]
 
-# a value of an attribute (= a Node in a decision tree)
-class Node:
-    # takes a distribution of the number of instances in each class
-    # eg. 3 "yes" and 2 "no" = [3,2]
-    def __init__(self, distribution = []):
-        self.distribution = distribution
-    # the total number of instances under this node
-    def count(self):
-        return sum(self.distribution)
-    # get the entropy at this node
-    def entropy(self):
-        # return the entropy for a list of probabilities for each class
-        return entropy(probs_from_counts(self.distribution))
+# a data point that has a feature (attribute) vector and a class
+class Instance:
+    def __init__(self, vector = {}, c = 0):
+        self.vector = vector
+        self.c = c
+    def getclass(self):
+        return self.c
+    def getvalue(self, attribute):
+        return self.vector[attribute]
 
-class Attribute:
-    # takes a list of distributions, each of which represents a value
-    # eg. for the first value, we have 3 "yes" and 2 "no", and for the second
-    # value we have 4 "yes" and 0 "no"; hence [[3,2],[4,0]]
-    def __init__(self, distributions = []):
-        # save the number of classes possible
-        if len(distributions) > 0:
-            self.num_classes = len(distributions[0])
-        else:
-            self.num_classes = 0
-        # convert the distributions to nodes and save them
-        self.nodes = [Node(distribution) for distribution in distributions]
-    # get the total number of instances in all the values of this attribute
-    def total(self):
-        return sum([node.count() for node in self.nodes])
-    # get the total number of instances in class c
-    # eg. total number of "yes" according to the prior convention can be
-    # gotten with self.count(0) - and number of "no" with self.count(1)
-    def count(self, c):
-        count = 0
-        for node in self.nodes:
-            count = count + node.distribution[c]
-        return count
-    # get the entropy before branching here
-    def entropy(self):
-        distribution = []
-        # get the distribution of total instances for each class
-        for c in range(self.num_classes):
-            distribution.append(self.count(c))
+# nodes should take the attributes so far and the data points
+class Node:
+    def __init__(self, attributes_remaining, instances):
+        self.attributes_remaining = attributes_remaining
+        self.instances = instances
+
+        # a dictionary of value -> node for the attribute we're splitting on
+        self.attribute = ''
+        self.children = {}
+    # the node can't be split anymore if all the instances have the same class
+    def finished(self):
+        # if there are no instances, then we're done
+        if len(self.instances) == 0:
+            return True
+
+        # check the class of all the instances
+        majority = instances[0].getclass()
+        for instance in self.instances[1:]:
+            # if an instance has a different class, then we're not finished
+            if instance.getclass() != majority:
+                return False
+        # otherwise, we're done
+        return True
+    # get the number of instances in each classes among this node's instances
+    def getcountdistribution(self, instances = None):
+        # default is the distribution of all of this node's instances
+        if instances == None:
+            instances = self.instances
+        # work out the count of each class
+        counts = defaultdict(int)
+        for instance in instances:
+            counts[instance.getclass()] += 1
+        return counts
+    # get the entropy at this node
+    def entropy(self, instances = None):
+        # default is the entropy of all of this node's instances
+        if instances == None:
+            instances = self.instances
         # return the entropy for a list of probabilities for each class
-        return entropy(probs_from_counts(distribution))
-    # get the mean information contained in this attribute
-    def meaninfo(self):
+        return entropy(probs_from_counts(\
+                        self.getcountdistribution(instances).values()\
+                    ))
+    # get the majority class at this node
+    def majorityclass(self):
+        # get the distribution of class memberships
+        distribution = self.getcountdistribution()
+        # go through each all their counts to find the majority
+        majorityclass = None
+        majoritycount = 0
+        for c in distribution.keys():
+            # if the current class beats the current majority
+            if distribution[c] > majoritycount:
+                # replace the majority with this one
+                majorityclass = c
+                majoritycount = distribution[c]
+        return majorityclass
+    # a testing function
+    def DFS(self, parents = []):
+        # perform DFS on its children
+        for value in self.children.keys():
+            self.children[value].DFS(parents + [self.attribute + '=' + value])
+        # if it's a leaf node; print its majority class and entropy
+        if len(self.children) == 0:
+            print ', '.join(parents)
+            print 'distribution: [' +\
+                ','.join(\
+                        [str(c) for c in self.getcountdistribution().values()]\
+                    ) +\
+                    ']'
+            print 'majority class: ' + self.majorityclass()
+            print 'entropy: ' + str(self.entropy())
+
+    # get the value distribution for if we split on the given attribute
+    def getdistributionforattribute(self, attribute):
+        # { value1 -> { class1 -> 5, class2 -> 3, ... }, ... }
+        values = {}
+        # for each instance in the current node
+        for instance in self.instances:
+            # get its value for this attribute
+            value = instance.getvalue(attribute)
+            # initialize this value's dictionary if needed
+            if value not in values:
+                values[value] = defaultdict(int)
+            # increment the count of this class
+            values[value][instance.getclass()] += 1
+        # return the distribution we found
+        return values
+    # get the mean information that the given attribute would have
+    def meaninfo(self, attribute):
         mean = 0
-        total = self.total()
-        # mean info = Sum[i=1->N](E[i]*P[i])
-        for i in range(len(self.nodes)):
-            node = self.nodes[i]
-            mean = mean + (node.entropy() * node.count() / total)
+        # get the distribution of values for this attribute
+        distribution = self.getdistributionforattribute(attribute)
+        # the number of this node's instances
+        total = len(self.instances)
+        # for each value in the distribution
+        for value in distribution.keys():
+            # find the instances that have that value
+            i = [instance for instance in self.instances \
+                    if instance.getvalue(attribute) == value]
+            # mean = Sum[Entropy * Probability] for all values
+            E = self.entropy(i)
+            P = len(i) / total
+            mean += E * P
+        # return the mean information
         return mean
-    # get the information gain from this attribute
-    def infogain(self):
-        return self.entropy() - self.meaninfo()
+    # perform the ID3 algorithm on this node
+    def ID3(self):
+        # if we're satisfied with what we have; stop
+        if self.finished():
+            return
+
+        # work out the entropy at this node
+        E = self.entropy()
+
+        # initially, we don't know what the best attribute is
+        best_attribute = None
+        best_infogain = 0
+
+        # for each attribute
+        for attribute in self.attributes_remaining:
+            # find its information gain
+            IG = E - self.meaninfo(attribute)
+            # if it beats what we have, keep it
+            if IG > best_infogain:
+                best_attribute = attribute
+                best_infogain = IG
+
+        # if we haven't found an attribute that gives an info gain; stop
+        if best_attribute == None:
+            return
+
+        # since we're splitting on that attribute, take it out of the new list
+        # of attributes remaining
+        attributes_remaining = [att for att in self.attributes_remaining \
+                                    if att != best_attribute]
+
+        # remember the attribute we're splitting on
+        self.attribute = best_attribute
+
+        # group the instances by their value of that attribute
+        # { value1 -> [instance1, instance2, ...], ... }
+        values = {}
+        for instance in self.instances:
+            # get the value of each instance
+            value = instance.getvalue(best_attribute)
+            # init a list for that value if needed
+            if value not in values:
+                values[value] = []
+            # add the instance to that value's list
+            values[value].append(instance)
+
+
+        # branch into child nodes
+        for value in values.keys():
+            # create a node for it
+            valueN = Node(attributes_remaining, values[value])
+            # add the new node to this node's children
+            self.children[value] = valueN
+            # run ID3 on it
+            valueN.ID3()
+
+# weather data
+instances = [\
+    Instance({ 'outlook' : 'sunny', 'temperature' : 'hot', 'humidity' : 'high', 'windy' : 'false' }, 'no'),\
+    Instance({ 'outlook' : 'sunny', 'temperature' : 'hot', 'humidity' : 'high', 'windy' : 'true' }, 'no'),\
+    \
+    Instance({ 'outlook' : 'overcast', 'temperature' : 'hot', 'humidity' : 'high', 'windy' : 'false' }, 'yes'),\
+    \
+    Instance({ 'outlook' : 'rainy', 'temperature' : 'mild', 'humidity' : 'high', 'windy' : 'false' }, 'yes'),\
+    Instance({ 'outlook' : 'rainy', 'temperature' : 'cool', 'humidity' : 'normal', 'windy' : 'false' }, 'yes'),\
+    Instance({ 'outlook' : 'rainy', 'temperature' : 'cool', 'humidity' : 'normal', 'windy' : 'true' }, 'no'),\
+    \
+    Instance({ 'outlook' : 'overcast', 'temperature' : 'cool', 'humidity' : 'normal', 'windy' : 'true' }, 'yes'),\
+    \
+    Instance({ 'outlook' : 'sunny', 'temperature' : 'mild', 'humidity' : 'high', 'windy' : 'false' }, 'no'),\
+    Instance({ 'outlook' : 'sunny', 'temperature' : 'cool', 'humidity' : 'normal', 'windy' : 'false' }, 'yes'),\
+    \
+    Instance({ 'outlook' : 'rainy', 'temperature' : 'mild', 'humidity' : 'normal', 'windy' : 'false' }, 'yes'),\
+    \
+    Instance({ 'outlook' : 'sunny', 'temperature' : 'mild', 'humidity' : 'normal', 'windy' : 'true' }, 'yes'),\
+    \
+    Instance({ 'outlook' : 'overcast', 'temperature' : 'mild', 'humidity' : 'high', 'windy' : 'true' }, 'yes'),\
+    Instance({ 'outlook' : 'overcast', 'temperature' : 'hot', 'humidity' : 'normal', 'windy' : 'false' }, 'yes'),\
+    \
+    Instance({ 'outlook' : 'rainy', 'temperature' : 'mild', 'humidity' : 'high', 'windy' : 'true' }, 'no'),\
+]
+attributes = ['outlook', 'temperature', 'humidity', 'windy']
+
+# test it out
+tree = Node(attributes, instances)
+tree.ID3()
+tree.DFS()
